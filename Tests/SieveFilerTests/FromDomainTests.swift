cf4c6ab15f45a7aab02b9cb68e7699dcc9abe810
Danny Sung <danny@dannysung.com>
2023-01-27 22:15:31 -0800

    FromDomainTests: Add more unit tests

--- Tests/SieveFilerTests/FromDomainTests.swift
+++ Tests/SieveFilerTests/FromDomainTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import SieveFiler
 
 final class FromDomainTests: XCTestCase {
-    func testThat_OneDomain_WillFile() throws {
+    func testThat_OneDomain_WillCreateOneFile() throws {
         let expectedValue = """
         if address :is :domain "From" [
             "domain.com"
@@ -23,7 +23,7 @@ final class FromDomainTests: XCTestCase {
         XCTAssertEqual(observedValue, expectedValue)
     }
 
-    func testThat_TwoDomains_WillFile() throws {
+    func testThat_TwoDomains_WillCreateOneFile() throws {
         let expectedValue = """
         if address :is :domain "From" [
             "somedomain.com",
@@ -45,4 +45,36 @@ final class FromDomainTests: XCTestCase {
         XCTAssertEqual(observedValue, expectedValue)
     }
 
+    func testThat_TwoFolders_WillCreateTwoFiles() throws {
+        let expectedValue = """
+        if address :is :domain "From" [
+            "domain1.com"
+        ] {
+            fileinto "Mailbox1";
+            stop;
+        }
+        if address :is :domain "From" [
+            "domain2.com"
+        ] {
+            fileinto "Mailbox2";
+            stop;
+        }
+        """
+
+        let observedValue = SieveRules {
+            Folder("Mailbox1") {
+                Fields(.from) {
+                    Domains("domain1.com")
+                }
+            }
+            Folder("Mailbox2") {
+                Fields(.from) {
+                    Domains("domain2.com")
+                }
+            }
+        }.asString
+
+        XCTAssertEqual(observedValue, expectedValue)
+
+    }
 }

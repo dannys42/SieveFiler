eede382963fd9e58835c8eec2e70e4669c784b59
Danny Sung <danny@dannysung.com>
2023-01-29 15:57:26 -0800

    Add AddressTests

---
+++ Tests/SieveFilerTests/AddressTests.swift
@@ -0,0 +1,34 @@
+//
+//  AddressTests.swift
+//  
+//
+//  Created by Danny Sung on 1/29/23.
+//
+
+import XCTest
+@testable import SieveFiler
+
+final class AddressTests: XCTestCase {
+
+    func testThat_OneAddress_WillFile() throws {
+        let expectedValue = """
+        if address :is :all "From" [
+            "someone@domain1.com"
+        ] {
+            fileinto "Mailbox1";
+            stop;
+        }
+        """
+
+        let observedValue = SieveRules {
+            Folder("Mailbox1") {
+                Fields(.from) {
+                    Addresses("someone@domain1.com")
+                }
+            }
+        }.asString
+
+        XCTAssertEqual(observedValue, expectedValue)
+    }
+
+}
